56e7bab92bac9f8a048666c579bbd7f7fe9e4bf7
juliagsy <67888047+juliagsy@users.noreply.github.com>
2024-01-25 19:16:55 +0000

    refactor card components

--- components/card/award.tsx
+++ components/card/award.tsx
@@ -1,18 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from '@/components/card/card.module.css';
+
 
 function AwardGroup({ item }) {
     return (
-        <div className='card'>
-            <div className='card-title'>
+        <div className={styles.card}>
+            <div className={styles.cardTitle}>
                 {item[1]}
             </div>
 
-            <div className='card-desc'>
+            <div className={styles.cardDesc}>
                 <p><FontAwesomeIcon icon="fa-solid fa-calendar-days" /> {item[0]}</p>
                 <p><FontAwesomeIcon icon="fa-solid fa-award" /> <a href={item[4]} target="_blank">{item[2]}</a></p>
             </div>
             
-            <div className='card-partition'></div>
+            <div className={styles.cardPartition}></div>
             <ul>
                 {
                     item[3].map((desc) => (
--- components/card/experience.tsx
+++ components/card/experience.tsx
@@ -1,22 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from '@/components/card/card.module.css';
+
 
 export default function Experience({ item }) { 
     return (
-        <div className='card'>
-            <div className='card-title'>
+        <div className={styles.card}>
+            <div className={styles.cardTitle}>
                 {item[1]}
             </div>
 
-            <div className='card-desc'>
+            <div className={styles.cardDesc}>
                 <p><FontAwesomeIcon icon="fa-solid fa-calendar-days" /> {item[0]}</p>
                 <p><FontAwesomeIcon icon="fa-solid fa-briefcase" /> <a href={item[3]} target="_blank">{item[2]}</a></p>
             </div>
             
-            <div className='card-partition'></div>
+            <div className={styles.cardPartition}></div>
             {/* <ul>
                 {
                     item[4].map((desc) => (
-                        <li><FontAwesomeIcon icon="fa-solid fa-chevron-right" /> {
+                        <li key={desc}><FontAwesomeIcon icon="fa-solid fa-chevron-right" /> {
                             Array.isArray(desc) ? <a href={desc[1]}>{desc[0]}</a> : desc
                         }</li>
                     ))
