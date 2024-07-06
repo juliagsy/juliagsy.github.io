600d50eb2fdf55e521d95461c35a524184ebfab7
juliagsy <67888047+juliagsy@users.noreply.github.com>
2024-07-06 11:15:05 +0800

    update qualification

--- components/card/education.tsx
+++ components/card/education.tsx
@@ -13,12 +13,13 @@ export default function Education({ item }) {
             <div className={styles.cardDesc}>
                 <p><FontAwesomeIcon icon={"fa-solid fa-calendar-days" as IconProp} /> {item[0]}</p>
                 <p><FontAwesomeIcon icon={"fa-solid fa-building-columns" as IconProp} /> <a href={item[4]} target="_blank">{item[2]}</a></p>
+                <p><FontAwesomeIcon icon={"fa-solid fa-award" as IconProp} /> {Array.isArray(item[3]) ? <a href={item[3][1]}>{item[3][0]}</a> : item[3]}</p>
             </div>
             
             <div className={styles.cardPartition}></div>
             <ul>
                 {
-                    item[3].map((desc) => (
+                    item[4].map((desc) => (
                         <li key={desc}><FontAwesomeIcon icon={"fa-solid fa-chevron-right" as IconProp} /> {
                             Array.isArray(desc) ? <a href={desc[1]}>{desc[0]}</a> : desc
                         }</li>
