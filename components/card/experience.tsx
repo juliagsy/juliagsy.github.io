09fd1204d7e255ee5e8e5b6d14dd6faded016719
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-02-06 10:24:51 +0800

    updates

--- components/card/experience.tsx
+++ components/card/experience.tsx
@@ -19,8 +19,10 @@ export default function Experience({ item }) {
             <ul>
                 {
                     item[4].slice(0, item[4].length - 1).map((desc) => (
-                        <li key={desc}><FontAwesomeIcon icon={"fa-solid fa-chevron-right" as IconProp} /> {
-                            Array.isArray(desc) ? <a href={desc[1]}>{desc[0]}</a> : desc
+                        <li key={desc}> {
+                            Array.isArray(desc) 
+                            ? <p className='customLink'><FontAwesomeIcon icon={"fa-solid fa-chevron-right" as IconProp} /> <a className="underline hover:text-violet-700" href={desc[0]}>{desc[1]}</a>: {desc[2]}</p>
+                            : <p><FontAwesomeIcon icon={"fa-solid fa-chevron-right" as IconProp} /> {desc}</p>
                         }</li>
                     ))
                 }
