bcd529ff9008fe5fe9234a7a836caa6b173ea7fc
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-03-18 13:59:14 +0800

    updates

--- components/about/profile.tsx
+++ components/about/profile.tsx
@@ -17,7 +17,8 @@ export default function Profile() {
                 <p className={`${style.item}`}><a href="/static/JuliaGoh_CVChi.pdf" target="_blank">Chinese</a></p>
             </div>
             <div className="flex flex-row gap-3">
-                <p><FontAwesomeIcon icon={"fa-solid fa-envelope-open-text" as IconProp} /> Reference Letters:</p>
+                <p><FontAwesomeIcon icon={"fa-solid fa-envelope-open-text" as IconProp} /> Referral Letters:</p>
+                <p className={`${style.item}`}><a href="/static/pdsb_ref.pdf" target="_blank">Petronas</a></p>
                 <p className={`${style.item}`}><a href="/static/unify_ref.pdf" target="_blank">Unify</a></p>
                 <p className={`${style.item}`}><a href="/static/ucl_ref.pdf" target="_blank">UCL</a></p>
             </div>
